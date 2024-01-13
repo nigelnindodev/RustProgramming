013d0c81e9eca3fb97aaaaa2af4443bf456e6b56
Nigel Nindo <nigelnindodev@gmail.com>
2024-01-13 09:27:11 +0300

    Complete basic Rust concepts

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let _tup: (i32, f64, u8) = (500, 6.4, 1);
 
     /*
-     * The line below use pattern mathcing to destructure the tuple we just created, which is
+     * The line below use pattern matching to destructure the tuple we just created, which is
      * really great that it's supported.
      */
     let (_x, _y, _z) = _tup;
@@ -42,9 +42,23 @@ fn main() {
 
     let added_value = plus_one(1);
     println!("The added value is {added_value}");
+
+    if_in_let_statement();
+
+    //infinite_loop();
+
+    return_values_from_loops();
+
+    condition_while_loop();
+
+    loop_collection();
+
+    loop_collection_for();
+
+    loop_range();
 }
 
-// Rust uses snake_case as convention for naming funtions
+// Rust uses snake_case as convention for naming functions
 fn another_function() {
     println!("Another function");
 }
@@ -58,4 +72,70 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 // Functions can also specify their return values with the -> syntax
 fn plus_one(number: i32) -> i32 {
     return number + 1;
+}
+
+// Because if is an expression, it an be used in the right side of a let statement to assign value
+// of a variable.
+fn if_in_let_statement() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+/*fn infinite_loop() {
+    loop {
+        println!("Again!");
+    }
+}*/
+
+// You can return a value after the break statement exiting the loop
+fn return_values_from_loops() {
+    let mut counter = 0;
+
+    let _result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The value of result is: {_result}");
+}
+
+fn condition_while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}");
+        number -= 1;
+    }
+
+    println!("Liftoff!");
+}
+
+fn loop_collection() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+}
+
+fn loop_collection_for() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("The value is {element}");
+    }
+}
+
+// Range is not inclusive at the end in Rust
+fn loop_range() {
+    for number in 1..4 {
+        println!("{number}!");
+    }
 }
